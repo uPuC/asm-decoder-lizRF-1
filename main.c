d62bb1acff387281e4a31ffee861d2106f205aa4
lizRF <132858052+lizRF@users.noreply.github.com>
2026-02-20 18:21:19 -0800

    p2_RamirezLizzette.c
    entrega 20 febrero practica 2

--- main.c
+++ main.c
@@ -15,7 +15,6 @@ const uint8_t flash_mem[] ={ /**simula la memoria*/
     0xE9, 0xF7, //0xF7E9 -> BRNE
     0x11, 0x97, //0x9711 -> SBIW
     0xC0, 0xE0, //0xE0C0 -> LDI
-
     0xD2, 0xE0, //0xE0D2 -> LDI
     0x09, 0x91, //0x9109 -> LD Y+
     0x1E, 0x91, //0x911E -> LD -X
@@ -24,7 +23,6 @@ const uint8_t flash_mem[] ={ /**simula la memoria*/
     0x0A, 0x2F, //0x2F0A -> MOV
     0x0A, 0x95, //0x950A -> DEC
     0x1C, 0x2F, //0x2F1C -> MOV
-
     0x01, 0x17, //0x1701 -> CP
     0xB9, 0xF7, //0xF7B9 -> BRNE
     0x0B, 0x2F, //0x2F0B -> MOV
@@ -36,8 +34,8 @@ const uint8_t flash_mem[] ={ /**simula la memoria*/
     };
 
 const uint16_t inst16_table[] = { /**almacena las instrucciones en su forma 'básica'*/
-    {0x0}, //NOP
-    {0x2400},  //CLR
+    {0x0000}, //NOP
+    {0x2400}, //CLR
     {0xE000}, //LDI
     {0x900D}, // LD +X
     {0x900E}, // LD -X
@@ -48,8 +46,7 @@ const uint16_t inst16_table[] = { /**almacena las instrucciones en su forma 'bá
     {0x1000}, //CP
     {0x2000}, //MOV
     {0x900A}, //DEC
-    {0x9003} //INC
-
+    {0x9003}  //INC
 };
 
 /**constantes que representan a las instrucciones del arreglo.*/
@@ -101,7 +98,7 @@ typedef union {
 int main()
 {
     Op_Code_t *instruction;
-    printf("- Practica 2: AVR OpCode -\n");
+    printf("*** Practica 2: AVR OpCode ***\n");
     // Decode the instructions by cycling through the array
     // procesa 2 bytes, es decir 1 instrucion
     for (uint8_t idx = 0; idx < sizeof(flash_mem); idx+=2)
@@ -111,8 +108,7 @@ int main()
         {
             printf("NOP\n");
         }
-        else if((instruction -> op16 &0xFC00)==inst16_table[e_CLR])
-        {
+        else if((instruction -> op16 &0xFC00)==inst16_table[e_CLR]){
             uint8_t d= instruction->type1.d10;
             //uint8_t op= instruction->type1.op6;
             printf("CLR R%d\n", d);
@@ -120,25 +116,25 @@ int main()
             uint8_t d = 16 + instruction->type2.d4; //se le suma 16 porque LDI trabaja con el R16 en adelante
             uint8_t k = (instruction->type2.k4_2 << 4) | instruction->type2.k4_1; //como K se divide en 2 mitades, se juntan con OR una vez desplazada la parte MSB
             printf("LDI R%d, 0x%d\n", d, k);
-        }else if((instruction-> op16 & 0xFE0F)== inst16_table[e_LDX]){
+        }else if((instruction-> op16 & 0xFE0F)== inst16_table[e_LDX]){ //
             uint8_t rd= instruction->type3.d5;
             printf("LD R%d, X+\n", rd);
-        }else if((instruction->op16 & 0xFE0F )== inst16_table [e_LDx]){
+        }else if((instruction->op16 & 0xFE0F )== inst16_table [e_LDx]){ //
             uint8_t rd = instruction->type3.d5;
             printf("LD R%d, -X\n", rd);
-        }else if((instruction->op16 & 0xFE0F)== inst16_table [e_LDY]){
+        }else if((instruction->op16 & 0xFE0F)== inst16_table [e_LDY]){ //
             uint8_t rd = instruction->type3.d5;
             printf("LD R%d, Y+\n", rd);
-        } else if((instruction->op16 & 0xF000)== inst16_table[e_CPI]){
+        } else if((instruction->op16 & 0xF000)== inst16_table[e_CPI]){ //
             uint8_t d = 16+ instruction->type2.d4;
             uint8_t k= (instruction->type2.k4_2<<4  | instruction->type2.k4_1);
             printf("CPI R%d, 0x%d\n", d, k);
-        } else if((instruction->op16 & 0xF000)== inst16_table [e_BRNE]){
+        } else if((instruction->op16 & 0xF000)== inst16_table [e_BRNE]){ //
             int8_t k= instruction->type4.k7;
             if (k >= 64) k= k- 128; //corregir signo si el bit mas significativo de k esta activo, quiere decir que es numero negativo
             //printf("BRNE PC %X\n", k);
             printf("BRNE PC %d\n", k+1);
-        }else if((instruction->op16 &0xFF00)== inst16_table[e_SBIW]){
+        }else if((instruction->op16 &0xFF00)== inst16_table[e_SBIW]){ //
             uint8_t d= (instruction->type5.d2 *2)+24;  //00 R24, 01 R26, 10 R28, 11 R30
             /*
              * si d=00, 00 (0d)*2=0+24=24
@@ -148,21 +144,21 @@ int main()
              */
             uint8_t k= (instruction->type5.k2_2<<4 | instruction->type5.k4_1);
             printf("SBIW R%d, %d\n", d, k);
-        }else if((instruction->op16 &0xF000)== inst16_table[e_CP]){
+        }else if((instruction->op16 &0xF000)== inst16_table[e_CP]){ //
             uint8_t d = (instruction->type6.d5);
             uint8_t k = (instruction->type6.r1_2 << 4) | instruction->type6.r4_1;
             printf("CP R%d, R%d\n",d, k);
         }
-        else if((instruction->op16 &0xF000)== inst16_table[e_MOV]){
+        else if((instruction->op16 &0xF000)== inst16_table[e_MOV]){ //
             uint8_t d= instruction->type6.d5;
             uint8_t r= (instruction->type6.r1_2 << 4) | instruction->type6.r4_1;
             printf("MOV R%d, R%d\n",d, r);
         }
-        else if((instruction->op16 &0xF00F)== inst16_table[e_DEC]){
+        else if((instruction->op16 &0xF00F)== inst16_table[e_DEC]){ //
             uint8_t d= instruction->type3.d5;
             printf("DEC R%d\n", d);
         }
-        else if((instruction->op16 &0xF00F)== inst16_table[e_INC]){
+        else if((instruction->op16 &0xF00F)== inst16_table[e_INC]){ //
             uint8_t d= instruction->type3.d5;
             printf("INC R%d\n", d);
         }
